68a4694e0e83889c372ca20fe82cc581b5df9b7a
RyoHotehama <hotehama1997@gmail.com>
2022-07-30 11:03:59 +0900

    軽微な修正

--- tomix-front/pages/layout/navigation.tsx
+++ tomix-front/pages/layout/navigation.tsx
@@ -8,7 +8,7 @@ const Navigation = () => {
     <Stack direction="row" spacing={8} sx={{justifyContent: 'center', borderBottom: 'solid 0.5px #797979'}}>
       <Button size="large">Primary</Button>
       <Button size="large">Link</Button>
-  </Stack>
+    </Stack>
   );
 }
 export default Navigation;
